3fe33b2db3fc771fe6bc59ddece019377092f104
kyrptonaught <anthony.butillo10@gmail.com>
2020-02-13 14:40:53 -0500

    Some clean up

--- src/main/java/net/kyrptonaught/commandlog/config/ConfigOptions.java
+++ src/main/java/net/kyrptonaught/commandlog/config/ConfigOptions.java
@@ -1,9 +1,14 @@
 package net.kyrptonaught.commandlog.config;
 
+import blue.endless.jankson.Comment;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ConfigOptions {
+
+    @Comment("https://www.google.com/search?q=java+datetime+format")
     public String DateTimeFormat = "M-dd-yyyy hh:mm:ss";
+    @Comment("List of commands you'd like to log, ie: \"/kill\"")
     public List<String> commands = new ArrayList<>();
 }
